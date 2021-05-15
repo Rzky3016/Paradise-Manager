59d3dcb2171413a23c0b0065e9ea4ee03bdfe74e
Yunus-ZEND <79404390+Yunus-ZEND@users.noreply.github.com>
2021-05-15 10:14:44 +0700

    Update afk.py

--- cinderella/modules/afk.py
+++ cinderella/modules/afk.py
@@ -27,7 +27,7 @@ def afk(bot: Bot, update: Update):
 
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
-    update.effective_message.reply_text("{} is now away!".format(fname))
+    update.effective_message.reply_text("{} sok sok an afk. Anak babi gitu!".format(fname))
 
     
 @run_async
@@ -47,7 +47,7 @@ def no_longer_afk(bot: Bot, update: Update):
         try:        
             options = [
           
-            'Anak asu si {} Sudah Kembali Online!'
+            '{} Sudah Kembali Online, makanya jgn sok sok an afk bodoh!'
      
                     ]
             chosen_option = random.choice(options)
